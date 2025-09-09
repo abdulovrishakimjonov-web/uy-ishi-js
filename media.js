dfed6b3f936005a8d9ec4453e3598922eb086178
Your Name <your.email@example.com>
2025-09-09 15:50:06 +0500

    uy-ishi-js

---
+++ media.js
@@ -0,0 +1,43 @@
+// let son = 40;
+
+// if (son == 10) {
+//     console.log("son 10 ga teng");   
+// } else if (son == 20) {
+//     console.log("son 20 ga teng");   
+// } else if (son == 30) {
+//     console.log("son 30 ga teng");   
+// } else {
+//     console.log("error");
+// }
+
+
+// let son = prompt("yoshingiz nechida yozing");
+
+// console.log(son);
+
+
+// 1
+// let son = +prompt("son kiriting");
+
+// if (son >= 0) {
+//     console.log(son + 1);
+// } else {
+//     console.log(son);
+// }
+
+//2
+
+
+// let score = +prompt(`bahoni kiriting`)
+
+//  if (100 > score) {
+//     console.log("malades bahoying bersh ")
+// }else if (89 === score ) {
+//    console.log(" yahshi bahoying tort");
+// }else if (69 === score ){
+//     console.log("qoniqrli bahoying uch");
+// }else if ( 49 === score ) {
+//     console.log("imtihondan yiqilding");
+// } else {
+//     console.log("malumot hato")
+// }
